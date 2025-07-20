e883f5aeed152a7ec2056689d9fec9c3ad3de729
EladEk-Amdocs <elade@amdocs.com>
2025-07-20 13:01:31 +0300

    fix slider

--- src/components/BackgroundSlider.js
+++ src/components/BackgroundSlider.js
@@ -1,25 +1,37 @@
-// src/components/BackgroundSlider.js
 import React, { useState, useEffect } from "react";
 import bg1 from "../img/1.jpg";
 import bg2 from "../img/2.jpg";
 import bg3 from "../img/3.jpg";
 import "./BackgroundSlider.css";
 
-export default function BackgroundSlider() {
-  const images = [bg1, bg2, bg3];
-  const [current, setCurrent] = useState(0);
+const images = [bg1, bg2, bg3];
 
+export default function BackgroundSlider() {
+  const [current, setCurrent] = useState(0);
+  const [loaded, setLoaded] = useState(true);
+
+  // Preload the next image and only show when loaded
+  useEffect(() => {
+    setLoaded(false);
+    const img = new window.Image();
+    img.onload = () => setLoaded(true);
+    img.src = images[current];
+  }, [current]);
+
+  // Change slide every 5 seconds
   useEffect(() => {
     const timer = setInterval(() => {
       setCurrent((c) => (c + 1) % images.length);
-    }, 5000); // change every 5s
+    }, 5000);
     return () => clearInterval(timer);
-  }, [images.length]);
+  }, []);
 
   return (
     <div
-      className="background-slider"
-      style={{ backgroundImage: `url(${images[current]})` }}
+      className={`background-slider${loaded ? " loaded" : ""}`}
+      style={{
+        backgroundImage: loaded ? `url(${images[current]})` : "none",
+      }}
     />
   );
 }
